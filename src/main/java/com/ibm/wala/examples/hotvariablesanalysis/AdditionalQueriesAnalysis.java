a27aa8253d5b0c474091571c790c5f25aaca9e59
fahadnayyar <fahad.nayyar10@gmail.com>
2021-07-24 03:33:40 +0530

    Initialized additional queries analysis files

---
+++ src/main/java/com/ibm/wala/examples/hotvariablesanalysis/AdditionalQueriesAnalysis.java
@@ -0,0 +1,131 @@
+/*
+ * Author: Fahad Nayyar (July, 2021)
+ *  TODO: add description of this class
+ */
+
+package com.ibm.wala.examples.hotvariablesanalysis;
+import com.ibm.wala.cfg.ControlFlowGraph;
+import com.ibm.wala.ssa.*;
+import com.ibm.wala.util.collections.Iterator2Iterable;
+import com.ibm.wala.util.collections.Pair;
+import java.util.*;
+
+public class AdditionalQueriesAnalysis {
+
+    // * helper methods
+    public static void println(Object str) {
+        System.out.println(str.toString());
+    }
+    public static void print(Object str) {
+        System.out.print(str.toString());
+    }
+
+    public static Pair < Map < ISSABasicBlock, Pair < Integer, Integer > >,
+            Map < SSAInstruction, Pair < Integer, Integer> > > perform(IR ir) {
+        println("starting!");
+        return  perform(ir.getControlFlowGraph(), ir.getSymbolTable());
+    }
+
+    public static Pair < Map < ISSABasicBlock, Pair < Integer, Integer > >,
+            Map < SSAInstruction, Pair < Integer, Integer> > > perform(
+            final ControlFlowGraph<SSAInstruction, ISSABasicBlock> cfg, final SymbolTable symtab) {
+
+        //* Array of all instructions in the cgf
+        final SSAInstruction[] instructions = cfg.getInstructions();
+
+        //* Initializing the flow sets.
+        Map < ISSABasicBlock, Pair < Integer, Integer> > flowSetsBB = new HashMap < ISSABasicBlock, Pair <Integer, Integer> >();
+        Map < SSAInstruction, Pair < Integer, Integer> > flowSetsInstr = new HashMap < SSAInstruction, Pair <Integer, Integer> >();
+
+        //* Initializing the visited map for bfs
+        Map<ISSABasicBlock, Integer> visitedMap = new HashMap<ISSABasicBlock, Integer>();
+
+        //* Iterating over all exit basic blocks of cfg to mark 0 in visited map, initialize flowSetsBB and flowSetsInst.
+//        println("Iterating over all exit basic blocks of cfg to mark 0 in visited map, initialize flowSetsBB and flowSetsInst");
+        Iterator<ISSABasicBlock> bbIterator = cfg.stream().iterator();
+        while (bbIterator.hasNext()) {
+            ISSABasicBlock currentBB = (ISSABasicBlock) bbIterator.next();
+            print("Iterating over Basic Block: "); println(bbIterator);
+            //* Mark not visited for currBB(0)
+            visitedMap.put(currentBB, new Integer(0));
+            //* Initialize entry and exit flow set of currBB to 0
+            Pair<Integer, Integer> currBBFlowSets = Pair.make(new Integer(0), new Integer(0));
+            flowSetsBB.put(currentBB, currBBFlowSets);
+            for (int i = currentBB.getLastInstructionIndex(); i >= currentBB.getFirstInstructionIndex(); i--) {
+                SSAInstruction currInstr = instructions[i];
+                //* Initialize entry and exit flow set of currInstr to 0
+                Pair<Integer, Integer> currInstrFlowSets = Pair.make(new Integer(0), new Integer(0));
+                flowSetsInstr.put(currInstr, currInstrFlowSets);
+            }
+        }
+
+        //* Level order traversal (bfs)
+//        println("Level order traversal (bfs):");
+        //* Initializing bfs queue
+        Queue<ISSABasicBlock> bfsQueue = new LinkedList<ISSABasicBlock>();
+        ISSABasicBlock exitBB = cfg.exit();
+        print("Exit basic block: "); println(exitBB);
+        //* Inserting exit basic block in bfs queue
+//        println("Inserting exit basic block in bfs queue");
+        bfsQueue.add(exitBB);
+        while(!bfsQueue.isEmpty()) {
+            ISSABasicBlock currentBB = (ISSABasicBlock) bfsQueue.remove();
+            //* Process the currentBB only if it was not visited earlier
+            if (visitedMap.get(currentBB) == 0) {
+                print("Visited BB: ");
+                println(currentBB.toString());
+                //* Mark currentBB as visited as it will be processed now
+                visitedMap.put(currentBB, new Integer(1));
+                //* Properly initializing the inValBB and outValBB
+                int inValBB = 0;
+                int outValBB = 0;
+                for (ISSABasicBlock succBB : Iterator2Iterable.make(cfg.getSuccNodes(currentBB))){
+                    int succInVal = flowSetsBB.get(succBB).fst;
+                    outValBB += succInVal;
+                }
+                inValBB = outValBB;
+                //* Iterating over the Instructions of this BB in reverse to update the flowsets:
+                for (int i = currentBB.getLastInstructionIndex(); i >= currentBB.getFirstInstructionIndex(); i--) {
+                    SSAInstruction currInstr = instructions[i];
+                    int outVal = 0;
+                    if (i!=currentBB.getLastInstructionIndex()) {
+                        outVal = flowSetsInstr.get(instructions[i+1]).fst;
+
+                    }else {
+                        for (ISSABasicBlock succBB : Iterator2Iterable.make(cfg.getSuccNodes(currentBB))){
+                            int succInVal = flowSetsBB.get(succBB).fst;
+                            outVal += succInVal;
+                        }
+                        outValBB = outVal;
+                    }
+                    int inVal = outVal;
+                    if (currInstr instanceof SSAConditionalBranchInstruction){
+                        inVal += 1;
+                    }
+                    inValBB = inVal;
+                    Pair<Integer, Integer> currInstrFlowSets = Pair.make(new Integer(inVal), new Integer(outVal));
+                    flowSetsInstr.put(currInstr, currInstrFlowSets);
+                }
+                Pair<Integer, Integer> currBBFlowSets = Pair.make(new Integer(inValBB), new Integer(outValBB));
+                flowSetsBB.put(currentBB, currBBFlowSets);
+                //* Pushing predecessors of currentBB into the bfs queue
+//                println("Pushing predecessors of currentBB into the bfs queue");
+                for (ISSABasicBlock predBB : Iterator2Iterable.make(cfg.getPredNodes(currentBB))) {
+                    if (visitedMap.get(predBB) == 0) {
+                        bfsQueue.add(predBB);
+                    }
+                }
+            }
+        }
+
+        //* DEBUGGING: Priting entry basic block
+        ISSABasicBlock entryBB = (ISSABasicBlock) cfg.entry();
+        print("Entry basic block: "); println(entryBB);
+
+        Pair < Map < ISSABasicBlock, Pair < Integer, Integer > >,
+                Map < SSAInstruction, Pair < Integer, Integer> > > returnPair = Pair.make(flowSetsBB, flowSetsInstr);
+
+        return returnPair;
+//        return perform(cfg, symtab, new BitVector());
+    }
+}
---
+++ src/main/java/com/ibm/wala/examples/hotvariablesanalysis/AdditionalQueriesAnalysisDriver.java
@@ -0,0 +1,137 @@
+/*
+ * Author: Fahad Nayyar (July, 2021)
+ *  TODO: add description of this class
+ */
+
+package com.ibm.wala.examples.hotvariablesanalysis;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import com.ibm.wala.classLoader.IClass;
+import com.ibm.wala.classLoader.IMethod;
+import com.ibm.wala.examples.drivers.ConstructAllIRs; // TODO: make a new file in this package
+import com.ibm.wala.ipa.callgraph.AnalysisCacheImpl;
+import com.ibm.wala.ipa.callgraph.AnalysisOptions;
+import com.ibm.wala.ipa.callgraph.AnalysisScope;
+import com.ibm.wala.ipa.callgraph.IAnalysisCacheView;
+import com.ibm.wala.ipa.callgraph.impl.Everywhere;
+import com.ibm.wala.ipa.cha.ClassHierarchy;
+import com.ibm.wala.ipa.cha.ClassHierarchyException;
+import com.ibm.wala.ipa.cha.ClassHierarchyFactory;
+import com.ibm.wala.ssa.ISSABasicBlock;
+import com.ibm.wala.ssa.SSAInstruction;
+import com.ibm.wala.util.collections.Pair;
+import com.ibm.wala.util.config.AnalysisScopeReader;
+import com.ibm.wala.util.perf.Stopwatch;
+import com.ibm.wala.examples.hotvariablesanalysis.TotalQueriesAnalysis;
+import com.ibm.wala.ssa.IR;
+
+/**
+ * An analysis skeleton that simply constructs IRs for all methods in a class hierarchy. Illustrates the use of
+ * {@link TotalQueriesAnalysis} to TODO: fill.
+ */
+public class AdditionalQueriesAnalysisDriver {
+
+    /**
+     * First command-line argument should be location of scope file for application to analyze
+     *
+     * @throws IOException
+     * @throws ClassHierarchyException
+     */
+    public static void main(String[] args) throws IOException, ClassHierarchyException {
+        //* TODO: add usage descripton.
+
+        String scopeFile = args[0];
+        String testCLassName = args[1];
+
+        // measure running time
+        Stopwatch s = new Stopwatch();
+        s.start();
+        AnalysisScope scope = AnalysisScopeReader.readJavaScope(scopeFile, null, ConstructAllIRs.class.getClassLoader());
+
+        // build a type hierarchy
+        System.out.print("building class hierarchy...");
+        ClassHierarchy cha = ClassHierarchyFactory.make(scope);
+        System.out.println("done");
+
+        // register class hierarchy and AnalysisCache with the reference cleanser, so that their soft references are appropriately wiped
+//        ReferenceCleanser.registerClassHierarchy(cha);
+        AnalysisOptions options = new AnalysisOptions();
+        IAnalysisCacheView cache = new AnalysisCacheImpl(options.getSSAOptions());
+
+        System.out.println("building IRs...");
+        for (IClass klass : cha) {
+            String testCasePath = "Lcom/ibm/wala/examples/testcases/" + testCLassName;
+            if (klass.getName().toString().compareTo(testCasePath)==0) {
+                // could also be written: if (klass.getSourceFileName().compareTo("Test1.java") == 0 )
+                System.out.println("yeah1!");
+                System.out.println(klass.getSourceFileName());
+                System.out.println(klass.getName().toString());
+            }else {
+//                System.out.println(klass.getSourceFileName());
+//                System.out.println(klass.getName().toString());
+                continue;
+            }
+            for (IMethod method : klass.getDeclaredMethods()) {
+                if (method.getName().toString().compareTo("main")==0) {
+                    System.out.println("yeah2!!");
+                    System.out.println(method.getName().toString());
+
+
+                    // Here: write code to be analyzed on main method in Test1.java file (Test1 class)
+                    // construct an IR
+                    IR ir = cache.getIR(method, Everywhere.EVERYWHERE);
+                    AdditionalQueriesAnalysis totalQueriesAnalysis = new AdditionalQueriesAnalysis();
+                    if (ir != null) {
+                        System.out.println(ir.toString());
+//                        System.out.println(totalQueriesAnalysis.perform(ir));
+                        Pair <Map<ISSABasicBlock, Pair< Integer, Integer >>,
+                                Map <SSAInstruction, Pair < Integer, Integer> > > returnPair = AdditionalQueriesAnalysis.perform(ir);
+
+                        Map < ISSABasicBlock, Pair < Integer, Integer> > flowSetsBB = returnPair.fst;
+                        Map < SSAInstruction, Pair < Integer, Integer> > flowSetsInstr = returnPair.snd;
+
+
+                        //* OUTPUT-DISPLAY: Printing final flow sets of instructions
+                        println("Printing final flow sets of instructions: ");
+                        for (Map.Entry < SSAInstruction, Pair < Integer, Integer> > mapElem : flowSetsInstr.entrySet()){
+                            Pair<Integer, Integer> currInstrFlowSets = (Pair<Integer, Integer>)mapElem.getValue();
+                            SSAInstruction currentInstr = mapElem.getKey();
+                            if (currentInstr!=null) {
+                                print("INSET: { "); print(currInstrFlowSets.fst); println(" }");
+                                print("INSTRUCTION: "); println(currentInstr.toString());
+                                print("OUTSET: { "); print(currInstrFlowSets.fst); println(" }");
+                            }
+                        }
+
+                        //* OUTPUT-DISPLAY: Printing final flow sets of Basic Blocks
+                        println("Printing final flow sets of Basic Blocks: ");
+                        for (Map.Entry < ISSABasicBlock, Pair < Integer, Integer> > mapElem : flowSetsBB.entrySet()){
+                            Pair<Integer, Integer> currInstrFlowSets = (Pair<Integer, Integer>)mapElem.getValue();
+                            ISSABasicBlock currentBB = mapElem.getKey();
+                            if (currentBB!=null) {
+                                print("INSET: { "); print(currInstrFlowSets.fst); println(" }");
+                                print("BASIC-BLOCK: "); println(currentBB.toString());
+                                print("OUTSET: { "); print(currInstrFlowSets.fst); println(" }");
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        System.out.println("done");
+        s.stop();
+        System.out.println("RUNNING TIME: " + s.getElapsedMillis());
+    }
+
+    // * helper methods
+    public static void println(Object str) {
+        System.out.println(str.toString());
+    }
+    public static void print(Object str) {
+        System.out.print(str.toString());
+    }
+
+}
